1081fc78d64323c3247594d869a1cbebd652a0c9
Charlie Carolan <charliecarolan1999@gmail.com>
2023-06-23 00:01:29 +0100

    feat: react native upgrade 71.4 -> 72.0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: [
-    '@react-native-community',
+    '@react-native',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
