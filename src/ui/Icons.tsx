e8bdd9571c0006a3b81dcdeecd625b40b4ce2377
Charlie Carolan <charliecarolan1999@gmail.com>
2023-07-17 00:27:18 +0100

    feat: camera icon

--- src/ui/Icons.tsx
+++ src/ui/Icons.tsx
@@ -147,3 +147,11 @@ export const Photo = createIcon(
     <Path d="M14 14l1 -1c.928 -.893 2.072 -.893 3 0l3 3" />
   </>,
 );
+
+export const Camera = createIcon(
+  <>
+    <Path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <Path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
+    <Path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+  </>,
+);
